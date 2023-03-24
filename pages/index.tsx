31af213ac9f68b1fd560d2dd1868f02dbe4c926a
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-24 17:00:13 -0300

    Add home page view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
-import { Typography } from "@mui/material";
+import { Card, CardHeader, Grid, Typography } from "@mui/material";
 
 import { Layout } from "@/components/layouts";
 
 export default function Home() {
   return (
-    <Layout>
-      <Typography variant="h1" color="secondary">
-        Hola Mundo
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Pending" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Completed" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
