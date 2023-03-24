a13c52bab800f07232bdcccfca9af708385535b5
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-24 09:31:53 -0300

    Set up Material UI themes

---
+++ themes/dark-theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    error: {
+      main: red.A400,
+    },
+  },
+
+  components: {},
+});
