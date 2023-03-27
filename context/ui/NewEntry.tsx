fec1c9ece817bb5a306c769d46793f18a50ae761
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-27 10:01:23 -0300

    Add new entries to EntriesContext

--- context/ui/NewEntry.tsx
+++ context/ui/NewEntry.tsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Box } from "@mui/system";
 import { Button, TextField } from "@mui/material";
 import AddIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import SaveOutlinedIcon from "@mui/icons-material/SaveAltOutlined";
 
+import { EntriesContext } from "../entries";
+
 export const NewEntry = () => {
   const [isAdding, setIsAdding] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [touched, setTouched] = useState(false);
 
+  const { addNewEntry } = useContext(EntriesContext);
+
   const onSave = () => {
     if (!inputValue) return;
 
-    console.log(inputValue);
+    addNewEntry(inputValue);
+    setInputValue("");
+    setTouched(false);
+    setIsAdding(false);
   };
 
   return isAdding ? (
