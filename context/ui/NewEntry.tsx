127778b95c3f89605af40e28fb25c94cdbc306f3
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-27 09:28:23 -0300

    Add events & states to the form

--- context/ui/NewEntry.tsx
+++ context/ui/NewEntry.tsx
@@ -1,34 +1,58 @@
+import { useState } from "react";
+
 import { Box } from "@mui/system";
 import { Button, TextField } from "@mui/material";
 import AddIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import SaveOutlinedIcon from "@mui/icons-material/SaveAltOutlined";
 
 export const NewEntry = () => {
-  return (
-    <>
-      <Button startIcon={<AddIcon />} fullWidth variant="outlined">
-        Add Task
-      </Button>
+  const [isAdding, setIsAdding] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [touched, setTouched] = useState(false);
 
+  const onSave = () => {
+    if (!inputValue) return;
+
+    console.log(inputValue);
+  };
+
+  return isAdding ? (
+    <>
       <TextField
         fullWidth
         sx={{ marginTop: 2, marginBottom: 1 }}
         autoFocus
         multiline
         label="New Task"
-        helperText="Enter a value"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        helperText={inputValue.length <= 0 && touched && "Enter a value"}
+        error={inputValue.length <= 0 && touched}
+        onBlur={() => setTouched(true)}
       />
 
       <Box display="flex" justifyContent="space-between">
-        <Button variant="text">Cancel</Button>
+        <Button variant="text" onClick={() => setIsAdding(false)}>
+          Cancel
+        </Button>
         <Button
           variant="outlined"
           color="secondary"
           endIcon={<SaveOutlinedIcon />}
+          onClick={onSave}
         >
           Save
         </Button>
       </Box>
     </>
+  ) : (
+    <Button
+      startIcon={<AddIcon />}
+      fullWidth
+      variant="outlined"
+      onClick={() => setIsAdding(true)}
+    >
+      Add Task
+    </Button>
   );
 };
